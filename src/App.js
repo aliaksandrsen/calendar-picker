9ac5684200db4080152808668637c51b161290ea
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-09-28 22:19:41 +0300

    add start layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-const App = () => {
-  return <div> hello</div>;
-};
+import React from "react";
+import "./App.css";
 
-export default App;
+const calendarDates = Array(31)
+  .fill(0)
+  .map((e, i) => i);
+
+export const App = () => {
+  return (
+    <>
+      <div className="date-chooser">
+        <button className="date-chooser-button">
+          Start Date <span>0</span>
+        </button>
+        <button className="date-chooser-button">
+          End Date <span>0</span>
+        </button>
+      </div>
+
+      <div className="calendar">
+        {calendarDates.map((day, index) => {
+          return <div className="calendar-day">{day + 1}</div>;
+        })}
+      </div>
+    </>
+  );
+};
